f377776e53f35f2fd0bbe8a3f611a1b8bcd42741
liangwenqi <liangwenqi@qdama.cn>
2023-06-09 09:55:34 +0800

    SwapPairs24

--- src/main/java/com/wenqi/demo01/node/SwapPairs24.java
+++ src/main/java/com/wenqi/demo01/node/SwapPairs24.java
@@ -5,6 +5,15 @@ package com.wenqi.demo01.node;
  * @date 2023/1/30
  */
 public class SwapPairs24 {
+    public static void main(String[] args) {
+        SwapPairs24 swapPairs24 = new SwapPairs24();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+
+        swapPairs24.swapPairs(head);
+    }
+
     public ListNode swapPairs(ListNode head) {
         ListNode start = new ListNode(0);
         start.next = head;
@@ -18,5 +27,38 @@ public class SwapPairs24 {
             temp = node1;
         }
         return start.next;
+    }
+
+    public ListNode swapPair2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummyHead = new ListNode(0);
+
+        int step = 1;
+
+        ListNode prePre = dummyHead;
+        ListNode pre = head;
+        prePre.next = pre;
+        ListNode p = head.next;
+
+        while (p != null) {
+            ListNode pNextTmp = p.next;
+            if (step % 2 == 1) {
+                prePre.next = p;
+                p.next = pre;
+                pre.next = pNextTmp;
+
+                prePre = p;
+            } else {
+                prePre = pre;
+                pre = p;
+            }
+            p = pNextTmp;
+            step++;
+        }
+
+        return dummyHead.next;
     }
 }
