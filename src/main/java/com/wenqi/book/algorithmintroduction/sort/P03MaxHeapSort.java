9d00e9173ce325f98e9c82f692b6c3b63e05bc00
liangwenqi <liangwenqi@qdama.cn>
2024-07-22 11:15:06 +0800

    algor_intro-堆排序

--- src/main/java/com/wenqi/book/algorithmintroduction/sort/P03MaxHeapSort.java
+++ src/main/java/com/wenqi/book/algorithmintroduction/sort/P03MaxHeapSort.java
@@ -4,15 +4,27 @@ import java.util.Arrays;
 
 /**
  * 最大堆排序
- *
+ * 堆排序内容: 1. 堆化 maxHeapify, 2. 构建堆 buildMaxHeap,  3. 堆排序 heapSort
  * @author Wenqi Liang
  * @date 2024/7/21
  */
 public class P03MaxHeapSort {
     public static void main(String[] args) {
 //        testMaxHeapify();
-        testBuildMaxHeap();
+//        testBuildMaxHeap();
+        testHeapSort();
     }
+
+
+    /**
+     * result: [1, 2, 3, 4, 7, 8, 9, 10, 14, 16]
+     */
+    private static void testHeapSort() {
+        int[] nums = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        heapSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
 
     /**
      * result: [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
@@ -20,7 +32,7 @@ public class P03MaxHeapSort {
     private static void testBuildMaxHeap() {
         int[] nums = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
         buildMaxHeap(nums);
-        System.out.println(Arrays.toString(nums));
+        System.out.println("result => " + Arrays.toString(nums));
     }
 
     /**
@@ -28,17 +40,30 @@ public class P03MaxHeapSort {
      */
     private static void testMaxHeapify() {
         int[] nums = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
-        maxHeapify(nums, 1);
+        maxHeapify(nums, nums.length, 1);
         System.out.println(Arrays.toString(nums));
+    }
+
+    public static void heapSort(int[] nums) {
+        buildMaxHeap(nums);
+        System.out.println("buildMaxHeap => " + Arrays.toString(nums));
+        int n = nums.length - 1;
+        while (n >= 0) {
+            int max = nums[0];
+            nums[0] = nums[n];
+            nums[n] = max;
+            maxHeapify(nums, n, 0);
+            n--;
+        }
     }
 
     /**
      * 给定数组构造最大堆结构, 从 n / 2 索引开始建造
      */
     public static void buildMaxHeap(int[] nums) {
-        int index = (nums.length - 1) / 2;
+        int index = nums.length / 2;
         while (index >= 0) {
-            maxHeapify(nums, index);
+            maxHeapify(nums, nums.length, index);
             index--;
         }
     }
@@ -46,16 +71,16 @@ public class P03MaxHeapSort {
     /**
      * 给定数组和索引, 以该索引作为父节点, 检查并实现最大堆化
      */
-    public static void maxHeapify(int[] nums, int i) {
+    public static void maxHeapify(int[] nums, int length, int i) {
         int left = 2 * i + 1;
         int right = 2 * i + 2;
         int large = i;
 
-        if (left < nums.length && nums[left] > nums[i]) {
+        if (left < length && nums[left] > nums[i]) {
             large = left;
         }
 
-        if (right < nums.length && nums[right] > nums[large]) {
+        if (right < length && nums[right] > nums[large]) {
             large = right;
         }
 
@@ -66,7 +91,7 @@ public class P03MaxHeapSort {
             nums[large] = temp;
 
             // 再次检查是否需要最大堆化
-            maxHeapify(nums, large);
+            maxHeapify(nums, length, large);
         }
     }
 }
