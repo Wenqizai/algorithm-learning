6962272be737d761bf0a3f4a43ca305c272041d5
liangwenqi <liangwenqi@qdama.cn>
2024-07-25 10:05:36 +0800

    algor_intro-Random快排

--- src/main/java/com/wenqi/book/algorithmintroduction/sort/P06RandomQuickSort.java
+++ src/main/java/com/wenqi/book/algorithmintroduction/sort/P06RandomQuickSort.java
@@ -2,24 +2,23 @@ package com.wenqi.book.algorithmintroduction.sort;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * 快速排序:
  * 1. 找一个数组中间数, right的数比他大, left的数比小
  * 2. 不断拆分数组直至数组right的数比left的数大
  * <p>
- * 本次实现中: 在 calPartition() 寻找中间数的方法中, 固定选取 nums[right] 作为比较的中间数.
- * 在最坏的情况下，时间复杂度为 O(n^2),
- * 如果选举中间数的元素是随机的话 {@link P06RandomQuickSort}，平均时间复杂度可能会达到 O(n * log n)
+ * 本次实现是 {@link P05QuickSort} 的改进版，只改变中间数的选取方式，优化其性能, 使其平均时间复杂度降低为 O(n * log n)
  *
  * @author liangwenqi
- * @date 2024/7/23
+ * @date 2024/7/25
  */
-public class P05QuickSort {
+public class P06RandomQuickSort {
 
     public static void main(String[] args) {
-        //testQuickSort();
-        testPartition();
+        testQuickSort();
+        //testRandomPartition();
     }
 
     private static void testQuickSort() {
@@ -27,60 +26,71 @@ public class P05QuickSort {
         int[] nums = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
         quickSort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
-
+        System.out.println();
 
         // test2: 中间数在 right 位置的数组
         int[] nums2 = {4, 1, 3, 2, 5};
         quickSort(nums2, 0, nums2.length - 1);
         System.out.println(Arrays.toString(nums2));
+        System.out.println();
 
         // test2: 中间数在 left 位置的数组
         int[] nums3 = {4, 3, 2, 5, 1};
         quickSort(nums3, 0, nums3.length - 1);
         System.out.println(Arrays.toString(nums3));
-
+        System.out.println();
 
         // test4: 两个元素的数组
         int[] nums4 = {3, 4};
         quickSort(nums4, 0, nums4.length - 1);
         System.out.println(Arrays.toString(nums4));
+        System.out.println();
 
         // test5: 元素相同的数组
         int[] nums5 = {3, 3, 3, 3, 3, 3};
         quickSort(nums5, 0, nums5.length - 1);
         System.out.println(Arrays.toString(nums5));
-
+        System.out.println();
 
         // test6: 元素顺序一致的数组
         int[] nums6 = {1, 2, 3, 4, 7, 8, 9, 10, 14, 16};
         quickSort(nums6, 0, nums6.length - 1);
         System.out.println(Arrays.toString(nums6));
+        System.out.println();
+
     }
 
-    private static void testPartition() {
+    private static void testRandomPartition() {
         // test1
         int[] nums = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
-        System.out.println("test1 => mid == 4: " + Objects.equals(calPartition(nums, 0, nums.length - 1), 4));
+        System.out.println("test1 => nums is " + Arrays.toString(nums) + " and calculate a mid :" + calRandomPartition(nums, 0, nums.length - 1));
+        System.out.println();
 
         // test2
         int[] nums2 = {4, 1, 3, 2, 5};
-        System.out.println("test2 => mid == 4: " + Objects.equals(calPartition(nums2, 0, nums2.length - 1), 4));
+        System.out.println("test2 =>  nums is " + Arrays.toString(nums2) + " and calculate a mid :" + calRandomPartition(nums2, 0, nums2.length - 1));
+        System.out.println();
 
         // test3
         int[] nums3 = {4, 3, 2, 5, 1};
-        System.out.println("test3 => mid == 0: " + Objects.equals(calPartition(nums3, 0, nums3.length - 1), 0));
+        System.out.println("test3 => nums is " + Arrays.toString(nums3) + " and calculate a mid :" + calRandomPartition(nums3, 0, nums3.length - 1));
+        System.out.println();
 
         // test4
         int[] nums4 = {3, 4};
-        System.out.println("test4 => mid == 1: " + Objects.equals(calPartition(nums4, 0, nums4.length - 1), 1));
+        System.out.println("test4 => nums is " + Arrays.toString(nums4) + " and  calculate a mid :" + calRandomPartition(nums4, 0, nums4.length - 1));
+        System.out.println();
 
         // test5
         int[] nums5 = {3, 3, 3, 3, 3, 3};
-        System.out.println("test5 => mid == 0: " + Objects.equals(calPartition(nums5, 0, nums5.length - 1), 0));
+        System.out.println("test5 => nums is " + Arrays.toString(nums5) + " and  calculate a mid :" + calRandomPartition(nums5, 0, nums5.length - 1));
+        System.out.println();
 
         // test6
         int[] nums6 = {1, 2, 3, 4, 7, 8, 9, 10, 14, 16};
-        System.out.println("test6 => mid == 9: " + Objects.equals(calPartition(nums6, 0, nums6.length - 1), 9));
+        System.out.println("test6 => nums is " + Arrays.toString(nums6) + " and  calculate a mid :" + calRandomPartition(nums6, 0, nums6.length - 1));
+        System.out.println();
+
     }
 
     /**
@@ -94,7 +104,7 @@ public class P05QuickSort {
      */
     private static void quickSort(int[] nums, int left, int right) {
         if (left < right) {
-            int mid = calPartition(nums, left, right);
+            int mid = calRandomPartition(nums, left, right);
             quickSort(nums, left, mid - 1);
             quickSort(nums, mid + 1, right);
         }
@@ -102,14 +112,22 @@ public class P05QuickSort {
 
     /**
      * 寻找中间数 mid
-     * 1. 设定比中间数小的数组范围 left - 1 ~ i (注意当数组中没有比 mid 小的数时, i = left - 1)
-     * 2. 设定比中间数大的数组范围 i + 1 ~ j (注意当数组中没有比 mid 大的数时, j = i + 1)
+     * 1. 在 left ~ right 中随机选取一个 random,  exchange(nums[random, right])
+     * 2. 设定比中间数小的数组范围 left - 1 ~ i (注意当数组中没有比 mid 小的数时, i = left - 1)
+     * 3. 设定比中间数大的数组范围 i + 1 ~ j (注意当数组中没有比 mid 大的数时, j = i + 1)
      */
-    private static int calPartition(int[] nums, int left, int right) {
+    private static int calRandomPartition(int[] nums, int left, int right) {
         // 随机取一个中间数
-        int dummy = nums[right];
+        int random = new Random().nextInt(right - left + 1) + left;
+        int dummy = nums[random];
+        nums[random] = nums[right];
+        nums[right] = dummy;
+
+        System.out.println("choose a random index :" + random + ", a num is : " + dummy + ", when left is " + left + " and right is " + right);
+
         // 比中间数小左下标边界
         int min = left - 1;
+
         // 比中间数大右下标边界
         int max = left;
 
